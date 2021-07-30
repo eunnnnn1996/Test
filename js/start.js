2160e1570014af97eeedc9107a48da09247b755c
eunnnnn1996 <eunnnnn1996@gmail.com>
2021-07-30 22:43:49 +0900

    95% 완성 7-30

--- js/start.js
+++ js/start.js
@@ -2,44 +2,28 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
 const endPoint = 7;
-const select = [];
+const select = [0,0,0,0,0,0,0,0,0,0];
 
 function calResult(){
-    var pointArray = [
-        { name: 'france', value: 0, key: 0},
-        { name: 'mongol', value: 0, key: 1},
-        { name: 'china', value: 0, key: 2},
-        { name: 'uk', value: 0, key: 3},
-        { name: 'korea', value: 0, key: 4},
-        { name: 'newzealand', value: 0, key: 5},
-        { name: 'vietnam', value: 0, key: 6},
-        { name: 'tiwan', value: 0, key: 7},
-        { name: 'usa', value: 0, key: 8},
-        { name: 'japan', value: 0, key: 9}
-    ]
-    for(let i = 0; i < endPoint; i++){
-        var target = qnaList[i].a[select[i]];
-        for(let j = 0; j<target.type.length; j++){
-            for(let k = 0; k < pointArray.length; k++){
-                if(target.type[j] === pointArray[k].name){
-                    pointArray[k].value +=1;
-                }
-            }
-        }
-    }
+    var result = select.indexOf(Math.max(...select));
+    return result;
+}
 
-    var resultArray = pointArray.sort(function (a,b){
-        if(a.value > b.value){
-            return -1;
-        }
-        if(a.value < b.value){
-            return 1;
-        }
-        return 0;
-    });
-    console.log(resultArray);
-    let resultword = resultArray[0].key;
-    return resultword;
+function setResult(){
+    let point = calResult();
+    const resultName = document.querySelector('.resultname');
+    resultName.innerHTML = infoList[point].name;
+
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult(){
@@ -53,7 +37,7 @@ function goResult(){
             result.style.display = "block"
         },450)
     })
-    calResult();
+    setResult();
 }
 
 function addAnswer(answerText,qIdx,idx){
@@ -75,7 +59,10 @@ function addAnswer(answerText,qIdx,idx){
             children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(() => {
-            select[qIdx] = idx;
+            var target = qnaList[qIdx].a[idx].type;
+        for(let i = 0; i<target.length; i++){
+            select[target[i]] +=1;
+        }
             for(let i = 0; i < children.length; i++){
                 children[i].style.display = 'none'; //클릭한 버튼 제외하고 나머지 사라짐
             }
